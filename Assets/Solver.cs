5f25f77fb82a783bddf2a406044bad2069a1a2c8
Bjartur Hafþórsson <bjarturhafthors95@gmail.com>
2018-10-22 14:02:18 +0200

    Small text changes

--- Assets/Solver.cs
+++ Assets/Solver.cs
@@ -7,7 +7,7 @@ public class Solver : MonoBehaviour
     public FingerController fingerController;
     public UIController uiController;
 
-    public void Solve()
+    public void BruteSolve()
     {
         float x = uiController.getXInput();
         float y = uiController.getYInput();
@@ -69,6 +69,48 @@ public class Solver : MonoBehaviour
         uiController.updateIPSlider(bestJ);
         fingerController.DPChanged(bestK, uiController.isIndependent());
         uiController.updateDPSlider(bestK);
+    }
+
+    public void JacobianIterative()
+    {
+        float degreeToRadianConstant = Mathf.PI / 180;
+
+        float mcpjGuess = 0;
+        float pipjGuess = 0;
+        float dipjGuess = 0;
+
+        float desiredX = 60;
+        float desiredY = -10;
+
+        int iterationCounter = 0;
+
+        float errorThreshold = 0.1f;
+
+        float currentX;
+        float currentY;
+
+        while (true)
+        {
+            currentX = 39.8f * Mathf.Cos(mcpjGuess * degreeToRadianConstant) + 22.4f * Mathf.Cos((mcpjGuess + pipjGuess) * degreeToRadianConstant) + 15.8f * Mathf.Cos((mcpjGuess + pipjGuess + dipjGuess) * degreeToRadianConstant);
+            currentY = 39.8f * Mathf.Sin(mcpjGuess * degreeToRadianConstant) + 22.4f * Mathf.Sin((mcpjGuess + pipjGuess) * degreeToRadianConstant) + 15.8f * Mathf.Sin((mcpjGuess + pipjGuess + dipjGuess) * degreeToRadianConstant);
+
+            if (iterationCounter == 25 || Mathf.Abs(desiredX - currentX) + Mathf.Abs(desiredY - currentY) < errorThreshold)
+            {
+                // Return solution
+            }
+
+            float oldMcpjGuess = mcpjGuess;
+            float oldPipjGuess = pipjGuess;
+            float oldDipjGuess = dipjGuess;
+
+            // Change guesses
+            mcpjGuess = 39.8f * -Mathf.Sin(oldMcpjGuess) + 22.4f * -Mathf.Sin(oldMcpjGuess + oldPipjGuess) + 15.8f * - Mathf.Sin(oldMcpjGuess + oldPipjGuess + oldDipjGuess);
+            pipjGuess = 22.4f * -Mathf.Sin(oldMcpjGuess + oldPipjGuess) + 15.8f * -Mathf.Sin(oldMcpjGuess + oldPipjGuess + oldDipjGuess);
+            dipjGuess = 15.8f * -Mathf.Sin(oldMcpjGuess + oldPipjGuess + oldDipjGuess);
+
+
+            iterationCounter++;
+        }
     }
 
 	// Use this for initialization
